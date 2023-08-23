3fbd59e1bd574196fc9eddff7c56e86f83378718
Amanda Matar <amandamatar@gmail.com>
2023-08-23 09:49:57 -0300

    estrutura de controle while

---
+++ estrutura-de-controle-while/estrutura_de_controle_while.js
@@ -0,0 +1,18 @@
+var numero = 1;
+
+while (numero < 10) {
+    console.log('numero', numero);
+    numero++;
+
+    if (numero === 5) {
+        break;
+    }
+
+    numero++;
+}
+
+/*
+while (true) {
+    console.log('Fica aqui para sempre!');
+}
+*/
