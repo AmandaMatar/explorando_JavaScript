6aba993156ca7eb5b713304e624d90370c44bbf5
Amanda Matar <amandamatar@gmail.com>
2023-08-23 15:16:49 -0300

    funções matematicas

---
+++ funçoes/funcoes_matematicas.js
@@ -0,0 +1,19 @@
+var menoNota = Math.min(3, 5, 1, 6, 10);
+console.log('Menor nota', menoNota);
+
+var maiorNota = Math.max(3, 5, 1, 6, 10);
+console.log('Maior nota', maiorNota);
+
+var temperatura = Math.round(22.3); //arredonda
+console.log('Temperatura', temperatura);
+
+var valorAleatorio = Math.random();
+console.log('Valor aleatorio', valorAleatorio);
+
+// mais exemplos de valor aleatorio
+
+var numero = valorAleatorio * 60;
+console.log('numero', numero);
+
+var numeroMega = Math.floor(4.2) + 1;
+console.log('numero da mega', numeroMega);
